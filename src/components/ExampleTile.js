d0924928f18653726797f9a7cbfbe2c495248f0b
Nick Taras <nicktaras@Nicks-MacBook-Pro-3.local>
2022-09-02 23:44:10 +1000

    cleaned example, link feature of react not in use.

--- src/components/ExampleTile.js
+++ src/components/ExampleTile.js
@@ -1,5 +1,4 @@
 import React from 'react'
-import {Link} from "react-router-dom";
 
 class ExampleTile extends React.Component {
 
@@ -8,14 +7,12 @@ class ExampleTile extends React.Component {
         console.log(this.props.example.demoUrl)
 
         return <div className="listTile">
-            {/* <Link to={"/details/" + this.props.urlKey}> */}
-                <a className='linkCard' href={this.props.example.demoUrl}>
-                    <img width={'100%'} src={this.props.example.thumbUrl}
-                         srcSet={this.props.example.thumbUrl.replace('.png', '') + '@2x.png 800w, ' + this.props.example.thumbUrl.replace('.png', '') + '@3x.png 1200w'}
-                         alt="Example thumbnail"/>
-                </a>
-                <h3 className="tileTitle" dangerouslySetInnerHTML={{__html: this.props.example.name.replace("(", "<br/>(")}}/>
-            {/* </Link> */}
+            <a className='linkCard' href={this.props.example.demoUrl}>
+                <img width={'100%'} src={this.props.example.thumbUrl}
+                        srcSet={this.props.example.thumbUrl.replace('.png', '') + '@2x.png 800w, ' + this.props.example.thumbUrl.replace('.png', '') + '@3x.png 1200w'}
+                        alt="Example thumbnail"/>
+            </a>
+            <h3 className="tileTitle" dangerouslySetInnerHTML={{__html: this.props.example.name.replace("(", "<br/>(")}}/>
         </div>;
     }
 }
